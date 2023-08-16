1190f969b911caae5523381826d366ec40c6a9bd
a4abash <abashshah@kbc.edu.np>
2023-08-16 11:39:26 +0545

    made changes in model

--- webmodels/admin.py
+++ webmodels/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Project, Post, ProImage
+from .models import Project, Post, Projectimage, Tag
+
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Post)
-admin.site.register(ProImage)
+admin.site.register(Projectimage)
+admin.site.register(Tag)
