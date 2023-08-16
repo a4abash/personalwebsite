cf317da045ac7c6028c9265b7c7efa1eea184ca3
a4abash <abashshah@kbc.edu.np>
2023-08-16 11:24:38 +0545

    added models

--- webmodels/admin.py
+++ webmodels/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project, Post, ProImage
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Post)
+admin.site.register(ProImage)
--- webmodels/models.py
+++ webmodels/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200,null=True,blank=True)
+    postimage = models.ImageField(null=True,blank=True,upload_to="images",default="images/blog.jpg")
+    body = models.TextField(null=True,blank=True)
+    tags = models.ManyToManyField(Tag,null=True)
+
+    def __str__(self):
+        return self.headline
+    
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    projectimage = models.ImageField(null=True,blank=True,upload_to="images",default="images/project.jpg")
+    subtitle = models.CharField(max_length=200,default='By Abash')
+    body = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
+#supports multiple image in the project
+class ProImage(models.Model):
+    image = models.ImageField(upload_to='kotha/')
+    projectImage = models.ForeignKey(Project,on_delete=models.SET_NULL,blank=True,null=True)
+
+    def __str__(self):
+        return self.image
